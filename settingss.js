f85e5d6a13e1a195ded22971a3b935db2fc27c6e
Renanvargas <renanluizvargasbr2005@gmail.com>
2025-12-23 14:39:42 -0300

    Refactor settings.js to export configuration object

--- settingss.js
+++ settingss.js
@@ -1,7 +1,15 @@
-// CONFIGURAÇÕES DO VemonBot2
+const settings = {
+    ownerName: 'Renan vargas',         // Seu nome
+    ownerNumber: '5532998665591',   // Seu número com DDI (Brasil = 55)
+    botName: 'VemonBot2',           // Nome do bot
+    prefix: '.',                    // Prefixo dos comandos (ex: .menu, .ping)
+    autoRead: true,                 // Marca mensagens como lidas automaticamente
+    autoreply: true,                // Responde automaticamente a mensagens
+    language: 'pt-br',              // Idioma padrão
+    antiCall: true,                 // Bloqueia quem tenta ligar
+    autorestart: true,              // Reinicia automaticamente em caso de erro
+    timezone: 'America/Sao_Paulo',  // Fuso horário
+    ownerMessage: 'Bot em operação. Contate o dono para suporte.'
+}
 
-module.exports = {
-  ownerNumber: ['5532998665591@s.whatsapp.net'],
-  botName: 'VemonBot2',
-  prefixPadrao: ['!', '.', '/']
-};
+module.exports = settings
