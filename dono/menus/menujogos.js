4961418ca0433fdb067fd5a94034d699d4152d59
Renanvargas <renanluizvargasbr2005@gmail.com>
2025-12-27 17:52:18 -0300

    Create menujogos.js

---
+++ dono/menus/menujogos.js
@@ -0,0 +1,29 @@
+const menujogos = (sender, prefix) => {
+return `༒W̷E̷L̷C̷O̷M̷E̷༒
+⇒ @${sender.split("@")[0]}
+
+『 𝐌𝐄𝐍𝐔✮𝐉𝐎𝐆𝐎𝐒 』
+╭════════════════════╯
+ | ೈ፝͜͡🎮 ${prefix}jogodavelha @
+ | ೈ፝͜͡🎮 ${prefix}rv
+ | ೈ፝͜͡🎮 ${prefix}akinator
+ | ೈ፝͜͡🎮 ${prefix}resetaki
+ | ೈ፝͜͡🎮 ${prefix}anagrama
+ | ೈ፝͜͡🎮 ${prefix}revelaranagrama
+ | ೈ፝͜͡🎮 ${prefix}quizanimais
+ | ೈ፝͜͡🎮 ${prefix}revelarquiz
+ | ೈ፝͜͡🎮 ${prefix}eununca
+ | ೈ፝͜͡🎮 ${prefix}vdddsf
+ | ೈ፝͜͡🎮 ${prefix}forca
+ | ೈ፝͜͡🎮 ${prefix}rfc
+ | ೈ፝͜͡🎮 ${prefix}campominado @
+ | ೈ፝͜͡🎮 ${prefix}resetmina
+ | ೈ፝͜͡🎮 ${prefix}cassino
+ | ೈ፝͜͡🎮 ${prefix}ppt
+ | ೈ፝͜͡🎮 ${prefix}dado
+ | ೈ፝͜͡🎮 ${prefix}caraoucoroa
+ | ೈ፝͜͡🎮 ${prefix}adivinharnmr
+╰════════════════════╮`
+}
+
+exports.menujogos = menujogos
